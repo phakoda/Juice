bf2cdf8bc44f29ff7a2f63072c649bbe2d49cd28
phakoda <130813466+phakoda@users.noreply.github.com>
2026-08-24 18:40:32 -0400

    Build physical keyboard forwarding

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -41,11 +41,12 @@ mkdir -p "$OUT"
   "$ROOT/app/main.m" "$ROOT/app/JuiceZip.m" "$ROOT/app/JuiceStoragePaths.m" \
   "$ROOT/app/JuicePrefixRepair.m" "$ROOT/app/JuiceApiSetBootstrap.m" \
   "$ROOT/app/JuiceWin32Bootstrap.m" "$ROOT/app/JuiceArchitectureRouting.m" \
-  "$ROOT/app/JuiceLogExport.m" "$ROOT/app/JuiceMultiWindowFix.m" \
-  "$ROOT/app/JuiceWindowActivation.m" "$ROOT/app/JuiceFramebufferFix.m" \
-  "$ROOT/app/JuiceBootProgress.m" "$ROOT/app/JuiceBootOverlayVisibility.m" \
-  "$ROOT/app/JuiceRuntimeHardening.m" "$ROOT/app/JuiceReconnectGrace.m" \
-  "$ROOT/app/JuiceGeometryHardening.m" "$ROOT/app/JuiceLaunchHardening.m" \
+  "$ROOT/app/JuiceHardwareKeyboard.m" "$ROOT/app/JuiceLogExport.m" \
+  "$ROOT/app/JuiceMultiWindowFix.m" "$ROOT/app/JuiceWindowActivation.m" \
+  "$ROOT/app/JuiceFramebufferFix.m" "$ROOT/app/JuiceBootProgress.m" \
+  "$ROOT/app/JuiceBootOverlayVisibility.m" "$ROOT/app/JuiceRuntimeHardening.m" \
+  "$ROOT/app/JuiceReconnectGrace.m" "$ROOT/app/JuiceGeometryHardening.m" \
+  "$ROOT/app/JuiceLaunchHardening.m" \
   -framework UIKit -framework Foundation -framework QuartzCore \
   -framework CoreGraphics -framework CoreFoundation -lz -o "$OUT/Juice"
 cp "$ROOT/config/Info.plist" "$OUT/Info.plist"
