ab537bfa0185f1f0926363779c8953953de84014
phakoda <130813466+phakoda@users.noreply.github.com>
2026-08-24 03:32:22 -0400

    Wire multi-window activation ordering

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -42,9 +42,9 @@ mkdir -p "$OUT"
   "$ROOT/app/JuiceApiSetBootstrap.m" "$ROOT/app/JuiceWin32Bootstrap.m" \
   "$ROOT/app/JuiceArchitectureRouting.m" "$ROOT/app/JuiceLegacyWin32.m" \
   "$ROOT/app/JuiceLogExport.m" "$ROOT/app/JuiceMultiWindowFix.m" \
-  "$ROOT/app/JuiceFramebufferFix.m" "$ROOT/app/JuiceBootProgress.m" \
-  "$ROOT/app/JuiceBootOverlayVisibility.m" "$ROOT/app/JuiceRuntimeHardening.m" \
-  "$ROOT/app/JuiceLaunchHardening.m" \
+  "$ROOT/app/JuiceWindowActivation.m" "$ROOT/app/JuiceFramebufferFix.m" \
+  "$ROOT/app/JuiceBootProgress.m" "$ROOT/app/JuiceBootOverlayVisibility.m" \
+  "$ROOT/app/JuiceRuntimeHardening.m" "$ROOT/app/JuiceLaunchHardening.m" \
   -framework UIKit -framework Foundation -framework QuartzCore \
   -framework CoreGraphics -framework CoreFoundation -lz -o "$OUT/Juice"
 cp "$ROOT/config/Info.plist" "$OUT/Info.plist"
