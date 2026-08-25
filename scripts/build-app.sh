4d52b4d6ec42a6d7d5941dc0bb374c4ad2c3ef02
phakoda <130813466+phakoda@users.noreply.github.com>
2026-08-25 15:53:37 -0400

    Wire reconnect and memory-pressure hardening into app build

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -46,7 +46,8 @@ mkdir -p "$OUT"
   "$ROOT/app/JuiceFramebufferFix.m" "$ROOT/app/JuiceBootProgress.m" \
   "$ROOT/app/JuiceBootOverlayVisibility.m" "$ROOT/app/JuiceSmokePath.m" \
   "$ROOT/app/JuiceSocketHardening.m" "$ROOT/app/JuiceHostIOHardening.m" \
-  "$ROOT/app/JuiceDisplayTransportHardening.m" "$ROOT/app/JuicePointerInput.m" \
+  "$ROOT/app/JuiceDisplayTransportHardening.m" "$ROOT/app/JuiceReconnectGrace.m" \
+  "$ROOT/app/JuicePointerInput.m" "$ROOT/app/JuiceMemoryPressure.m" \
   "$ROOT/app/JuiceLaunchHardening.m" \
   -framework UIKit -framework Foundation -framework QuartzCore -framework GameController \
   -framework CoreGraphics -framework Metal -lz -o "$OUT/Juice"
